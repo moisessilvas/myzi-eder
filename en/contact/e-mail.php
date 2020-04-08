e489d2500f992054a3402ec0996cf1f37a3098d6
Diego Oliveira <diegommobr@gmail.com>
2020-04-07 23:28:50 -0300

    Arrumando contatos de todas as newsletters

---
+++ en/contact/e-mail.php
@@ -0,0 +1,43 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+    require_once ('../../vendor/autoload.php');
+
+    function envio($email, $name, $subject, $text) {
+        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'ns904.hostgator.com.br';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'diegommobr@gmail.com';                 // SMTP username
+            $mail->Password = 'Tiatuke12';                           // SMTP password
+            $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 465;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('diegommobr@gmail.com', 'Equipe Mariana Menezes');
+            $mail->addAddress('emaildiegomo@gmail.com', "Website Contact");    // Add a recipient
+            $mail->addReplyTo($email, $name);
+            //$mail->addCC('emaildiegomo@gmail.com'); //para testar
+            //$mail->addBCC('bcc@example.com');
+
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $text;
+
+            $mail->send();
+        } catch (Exception $e) {
+            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
+    }
--- es/contacto/insert.php
+++ es/contacto/insert.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('../../administrator/pages/db.class.php');
+require_once('e-mail-gmail.php');
 
 $objDb = new db();
 $link = $objDb->conecta_mysql();
@@ -10,6 +11,8 @@ $email	= $_POST['email'];
 $sql = "INSERT INTO `email` (`idemail`, `email`) VALUES (NULL, '$email');";
 
 mysqli_query($link, $sql);
+envio_gmail_admin('Novo inscrito na newsletter', "O e-mail <b>$email</b> acabou de se inscrever para receber a newsletter de Mariana Menezes");
+envio_gmail_user($email, "Inscripción newsletter", "Gracias por inscribirse al newsletter!<br><br>Mariana Menezes")
 
 //$url = '../concertos/';
 //echo'<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
